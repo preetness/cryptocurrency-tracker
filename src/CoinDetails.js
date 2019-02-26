f391793b91ef54fe00041044c58a2c0055a0fe7d
Preet Dha <manpreetdha+github@gmail.com>
2019-02-26 13:40:21 -0800

    Add Victory for data visualization, and make API call to gather BitCoin prices for past month

--- src/CoinDetails.js
+++ src/CoinDetails.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { VictoryChart, VictoryTheme, VictoryLine } from 'victory';
 
 class CoinDetails extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        Prices for BitCoin in the past month:{' '}
+        {this.props.prices.map(price => {
+          return <li key={price.time}>{price.close}</li>;
+        })}
+      </div>
+    );
   }
 }
 
